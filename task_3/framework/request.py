67b2f1144945c65b3d1f9f5c1dd3eb0861655d34
Shorockhov-A <andr2409@bk.ru>
2022-05-27 10:01:07 +0300

    modified _parse_input_data() and _get_query_params()

--- task_3/framework/request.py
+++ task_3/framework/request.py
@@ -16,28 +16,34 @@ class Request:
                 headers[key[5:]] = value
         return headers
 
-    def _get_query_params(self, environ):
-        query_params = {}
-        data = environ['QUERY_STRING'].split('&')
-        for el in data:
-            if el:
-                key, value = el.split('=')
-                if query_params.get(key):
-                    query_params[key].append(value)
-                else:
-                    query_params[key] = [value]
-
-        return query_params
-
     def _parse_input_data(self, data: str):
         result = {}
         if data:
             params = data.split('&')
             for item in params:
-                key, value = item.split('=')
-                result[key] = value
+                if item:
+                    key, value = item.split('=')
+                    value = urllib.parse.unquote(value)
+                    if result.get(key):
+                        result[key].append(value)
+                    else:
+                        result[key] = [value]
 
         return result
+
+    def _get_query_params(self, environ):
+        query_params = self._parse_input_data(environ['QUERY_STRING'])
+        return query_params
+
+    # def _parse_input_data(self, data: str):
+    #     result = {}
+    #     if data:
+    #         params = data.split('&')
+    #         for item in params:
+    #             key, value = item.split('=')
+    #             result[key] = value
+    #
+    #     return result
 
     def _get_wsgi_input_data(self, environ):
         result = {}
@@ -46,6 +52,6 @@ class Request:
         data = environ['wsgi.input'].read(content_length) if content_length > 0 else b''
         if data:
             data_string = data.decode(encoding='utf-8').replace('+', ' ')
-            data_string = urllib.parse.unquote(data_string)
+            # data_string = urllib.parse.unquote(data_string)
             result = self._parse_input_data(data_string)
         return result
